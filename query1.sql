f7abebdd710eb98d4f19d8cc44f6e2bedcc9b79e
Baker Khoder <bakerkhoder878@gmail.com>
2022-09-11 17:05:38 +0300

    adding query2-6

--- query1.sql
+++ query1.sql
@@ -1,3 +1,3 @@
 .SELECT movies.name,movies.earnings_rank 
-FROM movies
- WHERE movies.id IN(SELECT movie_id FROM oscars) and movie.earnings_rank=(SELECT MIN(earnings_rank) FROM movies);
+FROM movies,oscars
+ WHERE movies.id IN(SELECT movie_id FROM oscars) AND movie.earnings_rank=(SELECT MIN(earnings_rank) FROM movies) AND oscars.type="Best-Picture";
