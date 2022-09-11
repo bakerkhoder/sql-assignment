428b7f35e15358a65bfb9cd3cd5cd54cf75244f1
Baker Khoder <bakerkhoder878@gmail.com>
2022-09-11 23:16:11 +0300

    fixing a query

--- query2-7.sql
+++ query2-7.sql
@@ -1,6 +1,7 @@
 SELECT COUNT(majorsin.student_id) 
-FROM majorsin,departments,enrolled 
-WHERE majorsin.department_id=departments.id AND majorsin.student_id=enrolled.student_id AND departments.name="CS";
+FROM majorsin,departments,enrolled,courses
+WHERE majorsin.department_id=departments.id AND majorsin.student_id=enrolled.student_id AND departments.name="CS"AND
+courses.crn=enrolled.course_crn;
 /* we did not use group by here because we are just asked for the number of students and nothing else"*/
 
 
